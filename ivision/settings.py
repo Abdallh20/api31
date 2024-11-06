f9ef6b713a0c0acaaa661a1d7bac12532e6ee81f
Abdallh20 <abdallhshref4@gmail.com>
2024-11-06 23:14:49 +0200

    dwadaw

--- ivision/settings.py
+++ ivision/settings.py
@@ -81,14 +81,14 @@ WSGI_APPLICATION = 'ivision.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',    
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'railway',
-        'USER':'postgres',
-        'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
-        'HOST':'autorack.proxy.rlwy.net',
-        'PORT':'58511',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',    
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME':'railway',
+    #     'USER':'postgres',
+    #     'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
+    #     'HOST':'autorack.proxy.rlwy.net',
+    #     'PORT':'58511',
     }
 }
 
