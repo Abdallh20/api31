da06e9138325b9d60685f5b85395590d5258c2c5
Abdallh20 <abdallhshref4@gmail.com>
2024-11-07 22:35:47 +0200

    dwd

--- ivision/settings.py
+++ ivision/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4wf7rclc2j+n+=cfa4p1s*sp!r^#=ul3nf0v=9!p6@#b7-jhaw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://api31-production.up.railway.app/','api31-production.up.railway.app','127.0.0.1']
+ALLOWED_HOSTS = ['https://api31-production.up.railway.app:8080/','api31-production.up.railway.app:8080','127.0.0.1']
 
 # Application definition
 
