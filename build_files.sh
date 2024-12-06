e6cac5cff0f5555b4e04a989a0296b5de3d9ff9b
Abdallh20 <abdallhshref4@gmail.com>
2024-12-06 18:15:42 +0200

    dda

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
 python3.9 manage.py collectstatic --no-input --clear
