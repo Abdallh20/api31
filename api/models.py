f8cebab7fa98355cdd815bc692c6778e05b9aa73
Abdallh20 <abdallhshref4@gmail.com>
2025-02-07 22:44:47 +0200

    sdwasdwsdwasdw

--- api/models.py
+++ api/models.py
@@ -11,7 +11,7 @@ class delocation(models.Model):
     is_fire = models.BooleanField()
     servo_angle=models.FloatField(default=0)
     target_angle=models.FloatField(default=0)
-    predict=models.FloatField(default=0)
+    predict=models.BooleanField(default=True)
     def __str__(self):
         return f"({self.longitude}, {self.latitude})"
 
