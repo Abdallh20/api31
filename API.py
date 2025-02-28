4d0c54332f583aeaec8772192b9dae0a30794d0b
Abdallh20 <abdallhshref4@gmail.com>
2025-02-28 16:45:05 +0200

    thisap[i

--- API.py
+++ API.py
@@ -3,8 +3,7 @@ import base64
 import numpy
 import time
 
-
-url = 'https://api31-3r4z.vercel.app/rest/'
+url = 'https://api31-six.vercel.app/rest/'
 response = requests.get(url)
 print(response)
 # headers = {
@@ -19,8 +18,6 @@ print(response)
 #     "is_fire": True
 # }
 # print(base64.encodebytes(image.tobytes()))
-print(f'*{str().join(['-' for i in range(117)])}*')
-print(f'| {'#':<5}| {'ID':<5}| {'Time':<19}| {'Average':<19}| {'Total':<19}| {'Status Code':<12}| {'Reasone':<25}|')
 times = []
 for i in range(20):
     response = requests.get(url)
